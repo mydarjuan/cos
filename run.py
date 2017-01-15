19d997a4b716af1caf5bd238a98f60d3e314b0b3
leoliu <350235397@qq.com>
2017-01-15 20:28:14 +0800

    add requirements.txt doc

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify, render_template
 
 from biz import userBiz
 from common import dbClient
@@ -6,9 +6,21 @@ from common import dbClient
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('/common/404.html', error), 404
+
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('/common/500.html', error), 500
+
+
 @app.route('/control-panel/index', methods=['GET'])
 def control_panel_index():
     return render_template('/admin/index.html')
+
+
 
 
 @app.route('/', methods=['GET'])
